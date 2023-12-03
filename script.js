41610adfd59f64ebd0b905a84aeb96cdc8bef10d
knilesh9 <neutron_stays@protonmail.com>
2023-12-03 15:40:44 +0530

    Done, Now, the cat's are appearing and after being removed

--- script.js
+++ script.js
@@ -1,7 +1,52 @@
 var insideRect = document.querySelector(".rectangle");
+const throttlePointerFn = (functionToDelay, delay) => {
+    let prev = 0;
+    return (...args) => {
+        let now = new Date().getTime();
+        if(now - prev > delay){
+        //will run the first time since time - 0 > delay
+            prev = now;
+            return functionToDelay(...args);
+        }
+    }
+}
 
-insideRect.addEventListener("mousemove", function(){
-    console.log("Mouse inside rect");
-} );
+insideRect.addEventListener("mousemove", 
+            throttlePointerFn((dets)=>{
+                var div = document.createElement("div");
+                div.classList.add("imageDiv");
+                //assigning class imageDiv to div above
 
-console.log();
+                div.style.left = dets.clientX + 'px';
+                div.style.top = dets.clientY + 'px';
+
+                var img = document.createElement("img");
+
+                img.setAttribute("src", "./media/picture/confusedCat.png");
+
+                div.appendChild(img);
+                document.body.appendChild(div);
+
+                gsap.to(img, {
+                    y: "0",
+                    ease: Power3,
+                    duration: .1
+                });
+
+                gsap.to(img, {
+                    y: "100%",
+                    ease: Power2,
+                    delay: .5,
+                });
+
+                console.log("Working");
+
+                setTimeout(function(){
+                    div.remove();
+                }, 1000)
+                //must have, else the cat's will stop
+                // to appear since last div's will take
+                // up all the space.
+
+            }, 100));
+
